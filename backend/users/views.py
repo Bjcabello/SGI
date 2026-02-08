0810125dc652ca62dc539c3b2679e870e3e54989
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2026-02-08 14:40:07 -0500

    views users

--- backend/users/views.py
+++ backend/users/views.py
@@ -1,3 +1,16 @@
-from django.shortcuts import render
+from rest_framework import generics, permissions
+from rest_framework.response import Response
+from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserSerializer, RegisterSerializer
+from .models import User
 
-# Create your views here.
+class RegisterView(generics.CreateAPIView):
+    permission_classes = [permissions.AllowAny]
+    serializer_class = RegisterSerializer
+
+class UserProfileView(generics.RetrieveUpdateAPIView):
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+    
+    def get_object(self):
+        return self.request.user
