d9216ad71da434ecf3e62cfaf3e339f23286a5fd
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2026-02-08 14:35:02 -0500

    serializers de inventory

---
+++ backend/inventory/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from .models import Product, StockMovement
+
+class ProductSerializer(serializers.ModelSerializer):
+    category_name = serializers.CharField(source='category.name', read_only=True)
+    supplier_name = serializers.CharField(source='supplier.name', read_only=True)
+    
+    class Meta:
+        model = Product
+        fields = '__all__'
+
+class StockMovementSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only=True)
+    created_by_username = serializers.CharField(source='created_by.username', read_only=True)
+    
+    class Meta:
+        model = StockMovement
+        fields = '__all__'
+        read_only_fields = ['created_by']
