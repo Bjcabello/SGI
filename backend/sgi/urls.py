18721392e4e437bb8880b367069c017234f59322
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2026-02-08 14:55:52 -0500

    settings y urls configuracion

--- backend/sgi/urls.py
+++ backend/sgi/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework.routers import DefaultRouter
 from categories.views import CategoryViewSet
 from suppliers.views import SupplierViewSet
@@ -17,3 +19,6 @@ urlpatterns = [
     path('api/auth/', include('users.urls')),
     path('api/', include(router.urls)),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
