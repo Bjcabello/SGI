d6b250e2a4d4e6e3b221bbf222b378c476d17e63
Bryan Joel Cabello Mero <bjcabello55@gmail.com>
2026-02-08 14:46:03 -0500

    views inventory

--- backend/inventory/views.py
+++ backend/inventory/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import viewsets, status
+from rest_framework.decorators import action
+from rest_framework.response import Response
+from django.db.models import F
+from .models import Product, StockMovement
+from .serializers import ProductSerializer, StockMovementSerializer
 
-# Create your views here.
+class ProductViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    
+    @action(detail=False, methods=['get'])
+    def low_stock(self, request):
+        products = Product.objects.filter(quantity__lte=F('min_stock'))
+        serializer = self.get_serializer(products, many=True)
+        return Response(serializer.data)
+
+class StockMovementViewSet(viewsets.ModelViewSet):
+    queryset = StockMovement.objects.all()
+    serializer_class = StockMovementSerializer
+    
+    def perform_create(self, serializer):
+        serializer.save(created_by=self.request.user)
